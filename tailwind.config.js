2d5360d21f129275beb1bf0031afe6bb6732fadd
saiNaidu6563 <saitammabbatula1233@gmail.com>
2025-04-21 12:40:19 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        scrollX: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        'scroll-x': 'scrollX 40s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+}
